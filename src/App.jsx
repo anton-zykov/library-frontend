3c59a3184e40d9e0b33bc57f29a9685f1043c010
Anton Zykov <anton.zykov1@gmail.com>
2023-06-07 19:20:03 +0300

    8.18 completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,44 @@ import { HashRouter, Route, Routes, Link } from 'react-router-dom';
 import Authors from './components/Authors';
 import Books from './components/Books';
 import NewBook from './components/NewBook';
+import Login from './components/Login';
 
 import styles from './App.module.scss';
+import { useApolloClient } from '@apollo/client';
 
-const App = () => (
-  <HashRouter>
-    <div className={styles.navigation}>
-      <Link to="/authors">authors</Link>
-      <Link to="/books">books</Link>
-      <Link to="/add">add book</Link>
-    </div>
+const App = () => {
+  const [token, setToken] = React.useState(null);
+  const client = useApolloClient();
 
-    <Routes>
-      <Route path="/authors" element={<Authors />} />
-      <Route path="/books" element={<Books />} />
-      <Route path="/add" element={<NewBook />} />
-    </Routes>
-  </HashRouter>
-);
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  return (
+    <HashRouter>
+      <div className={styles.navigation}>
+        <Link to="/authors"><button>authors</button></Link>
+        <Link to="/books"><button>books</button></Link>
+        {token && <Link to="/add"><button>add book</button></Link>}
+        {token && <button onClick={logout}>logout</button>}
+        {!token && <Link to="/login"><button>login</button></Link>}
+      </div>
+
+      <Routes>
+        <Route path="/authors" element={<Authors />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/add" element={<NewBook />} />
+        <Route
+          path="/login"
+          element={
+            <Login setError={() => {}} setToken={setToken} />
+          }
+        />
+      </Routes>
+    </HashRouter>
+  );
+};
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,28 @@ import App from './App';
 import {
   ApolloClient,
   ApolloProvider,
+  createHttpLink,
   InMemoryCache,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const authLink = setContext((_, context) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...context.headers,
+      authorization: token ? `Bearer ${token}` : null
+    }
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
